e14d739396fa855e460e92e65c92af0bf66c3ccd
patrickcrocker <patrick.crocker@gmail.com>
2016-10-05 16:52:47 -0500

    added separate pivnet and opsman scripts

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo install pivnet-download /usr/local/bin
 sudo install pivnet-upload /usr/local/bin
 sudo install uaac-login /usr/local/bin
 sudo install opsman /usr/local/bin
+sudo install pivnet /usr/local/bin
