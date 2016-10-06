6399cc046db3099fd767aee449bdddd37ce49381
patrickcrocker <patrick.crocker@gmail.com>
2016-10-05 20:00:41 -0500

    removed pivnet-download pivnet-upload uaac-login

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-sudo install pivnet-download /usr/local/bin
-sudo install pivnet-upload /usr/local/bin
-sudo install uaac-login /usr/local/bin
 sudo install opsman /usr/local/bin
 sudo install pivnet /usr/local/bin
